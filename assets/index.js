f60a712dcfe80eb56ab5d2525b46ba0f2ede3692
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-30 23:53:47 +0300

    Assignment 5 - Dictionary

--- assets/index.js
+++ assets/index.js
@@ -16,3 +16,41 @@ function fetchApi(word) {
     .then((res) => res.json())
     .then((result) => data(result, word));
 }
+
+searchInput.addEventListener("keyup", (e) => {
+  if (e.target.value && e.key === "Enter") {
+    fetchApi(e.target.value);
+  }
+});
+
+const data = (result, word) => {
+  console.log(result);
+  if (result.title) {
+    infoText.innerHTML = `Oops! No Definitions Found for ${word}`;
+  } else {
+    wrapper.classList.add("active");
+    let definitions = result[0].meanings[0].definitions[0].definition;
+
+    document.querySelector("#wrd").innerHTML = result[0].word;
+    let phonetic = result[0].phonetics[0].text;
+    let synonym = result[0].meanings[0].synonyms[0];
+    document.getElementById("meaning").innerHTML = definitions;
+    document.getElementById(
+      "phn"
+    ).innerHTML = `Commonly pronounced as ${phonetic}`;
+    document.querySelector(".example span").innerHTML = definitions.example
+      ? definitions.example
+      : "No example here.";
+
+      
+  }
+};
+
+removeIcon.addEventListener("click", (e) => {
+  searchInput.value = "";
+  searchInput.focus();
+  infoText.style.color = "#9a9a9a";
+  wrapper.classList.remove("active");
+  infoText.innerHTML =
+    " Type any existing word and press enter to get meaning, example,synonyms, etc.";
+});
